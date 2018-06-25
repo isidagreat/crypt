ce08a116ac56302549541ade39e735ad4d7db87a
isidagreat <isidagreat@gmail.com>
2018-06-25 12:34:48 -0700

    Added http service

---
+++ public/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ItempageComponent } from './itempage/itempage.component';
+import { HomeComponent } from './home/home.component'
+
+const routes: Routes = [
+  {path: 'item', component:ItempageComponent},
+  {path: 'home', component:HomeComponent },
+  {path: '', pathMatch:'full', redirectTo:'pets'}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
